768e17b4d25b1d4cd326bfb50a0c05ac6a44c777
mgcarbonell <mgcarbonell@gmail.com>
2022-01-31 13:21:55 -0800

    Completes challenge 1, attempts to make a main branch to delete master.

--- WriteYourFirstCSharpCode/Program.cs
+++ WriteYourFirstCSharpCode/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace WriteYourFirstCSharpCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Challenge1();
+        }
+        public static void Challenge1()
+        {
+            // Using two diffrerent techniques to print two lines of output, display the following:
+            // This is the first line.
+            // This is the second line.
+            Console.WriteLine("This is the first line.");
+            Console.WriteLine("This is the second line.");
+            Console.WriteLine("This is the first line. \nThis is the second line.");
+        }
+    }
+}
