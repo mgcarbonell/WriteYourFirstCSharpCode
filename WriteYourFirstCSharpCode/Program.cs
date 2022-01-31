2e576e9def3c273203776e855caf3b3b83d8dadc
mgcarbonell <mgcarbonell@gmail.com>
2022-01-31 14:23:45 -0800

    Works through variables

--- WriteYourFirstCSharpCode/Program.cs
+++ WriteYourFirstCSharpCode/Program.cs
@@ -6,7 +6,8 @@ namespace WriteYourFirstCSharpCode
     {
         static void Main(string[] args)
         {
-            Challenge1();
+            //Challenge1();
+            LiteralValues();
         }
         public static void Challenge1()
         {
@@ -16,6 +17,32 @@ namespace WriteYourFirstCSharpCode
             Console.WriteLine("This is the first line.");
             Console.WriteLine("This is the second line.");
             Console.WriteLine("This is the first line. \nThis is the second line.");
+        }
+        public static void LiteralValues()
+        {
+            // Why does
+            Console.WriteLine('b'); // => 'b'
+            // but why does the line of code below return
+            // Console.WriteLine('Hello World!');
+            // error: Too many characters in character literal
+            // '' stores a char, or character. 
+            // there are also int literals
+            int intLiteral = 123;
+            Console.WriteLine(intLiteral); // => 123
+            // and decimal literals
+            decimal decLiteral = 12.3M;
+            Console.WriteLine(decLiteral); // => 12.3
+            // and bool literals
+            bool boolLiteral = true;
+            Console.WriteLine(boolLiteral); // => true
+            Console.WriteLine(!boolLiteral); // => false
+
+        }
+        public static void WorkingWithVars()
+        {
+            string firstName; // assigning a variable
+            firstName = "Mario"; // "setting" the variable. 
+            Console.WriteLine(firstName); // "getting" the variable
         }
     }
 }
